b89baf6540f560eab1024733bcf6e95a7b92d36d
Axel Gabriel Calle Granda <97carmine@protonmail.com>
2021-09-03 16:54:32 +0200

    Added again the type deleted by accident

--- src/common/languages/messages/index.ts
+++ src/common/languages/messages/index.ts
@@ -1,6 +1,6 @@
 import messages_en from "./en.json";
 import messages_es from "./es.json";
 
-const messages = { en: messages_en, es: messages_es };
+const messages: any = { en: messages_en, es: messages_es };
 
 export default messages;
