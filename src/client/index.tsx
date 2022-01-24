359180c2fe956382d28c1e4861639e168f609ede
97carmine <axel.c.granda@gmail.com>
2022-01-24 22:41:34 +0100

    Converted to async function

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { hydrate } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { MessageFormatElement } from "react-intl";
 import LanguageProvider from "../common/providers/languageProvider";
 import App from "../common/components/app";
 import { obtainLanguage } from "../common/languages/utils";
-import { MessageFormatElement } from "react-intl";
 
 if (process.env.NODE_ENV !== "production") {
 	import("@axe-core/react")
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "production") {
 		.catch((error: Error) => console.log(`Error importing module @axe-core/react: ${error.message}`));
 }
 
-const loadMessages = (language: string): Promise<Record<string, MessageFormatElement[]>> => {
+const loadMessages = async (language: string): Promise<Record<string, MessageFormatElement[]>> => {
 	switch (language) {
 		case "es":
 			return import("../common/languages/messages/es.json").then((data) => data.default);
