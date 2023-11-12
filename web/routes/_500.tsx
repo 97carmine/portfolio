a70f13564a0d987b0260ae718ea60973c93dc201
97carmine <axel.c.granda@gmail.com>
2023-11-12 13:39:14 +0100

    Update 500 error page

--- web/routes/_500.tsx
+++ web/routes/_500.tsx
@@ -1,17 +1,32 @@
-import { PageProps } from "$fresh/server.ts";
-import { IS_BROWSER } from "$fresh/runtime.ts";
 import Page from "~/components/page.tsx";
+import { asset } from "$fresh/runtime.ts";
 
-export default (props: PageProps) => {
+export default () => {
   const meta = {
     title: "Error 500",
-    canonical: IS_BROWSER ? document.location.href : props.url.href,
-    description: "Error",
+    description: "Page not found",
   };
 
   return (
     <Page {...meta}>
       <section class="section">
+        <div class="container">
+          <article>
+            <h1 class="title is-spaced">Error 500</h1>
+            <h2 class="subtitle">
+              Well, this is a bit embarrassing. Something went wrong.
+            </h2>
+            <div class="content">
+              <p>It's time to call the cavalry, ladies and gentlemen, in charge of solving this problem, Aurora.</p>
+              <img
+                width="200"
+                height="200"
+                src={asset("/images/support.webp")}
+                alt="Cat called Aurora"
+              />
+            </div>
+          </article>
+        </div>
       </section>
     </Page>
   );
